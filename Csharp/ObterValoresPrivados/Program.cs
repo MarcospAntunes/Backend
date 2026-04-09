47bf52610b1afb289509a119baeee026ee81f6e6
MarcospAntunes <marcospantunes203@gmail.com>
2026-04-09 15:43:25 -0300

    Como acessar os valores das variaveis privadas de uma classe

---
+++ Csharp/ObterValoresPrivados/Program.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+
+class HasASecret
+{
+  private string secret = "xyzzy";
+}
+
+class Program
+{
+  public static void Main(string[] args)
+  {
+    HasASecret keeper = new HasASecret();
+
+    FieldInfo[] fields = keeper.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+    foreach (FieldInfo field in fields)
+    {
+      Console.WriteLine(field.GetValue(keeper));
+    }
+  }
+}
